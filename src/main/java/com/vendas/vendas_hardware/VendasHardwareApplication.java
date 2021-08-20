e056861ce68744f179a331a88665ad5f26c15075
jntd <jntd@gft.com>
2021-08-19 22:08:49 -0300

    classe produto criada

---
+++ src/main/java/com/vendas/vendas_hardware/VendasHardwareApplication.java
@@ -0,0 +1,14 @@
+package com.vendas.vendas_hardware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasHardwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasHardwareApplication.class, args);
+		System.out.println("Projeto iniciado");
+	}
+
+}
---
+++ src/test/java/com/vendas/vendas_hardware/VendasHardwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendas.vendas_hardware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasHardwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
