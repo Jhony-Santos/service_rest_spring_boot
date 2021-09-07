8ae15ce0a70110edd7d930edb0d6f548b4412303
jntd <jntd@gft.com>
2021-09-07 11:43:24 -0300

    projeto reestruturado

---
+++ src/test/java/com/example/E_Commerce/E_Commerce.java
@@ -0,0 +1,13 @@
+package com.example.sistemaControle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaControleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
