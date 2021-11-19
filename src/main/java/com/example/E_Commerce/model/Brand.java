f8db63a56bf9e5e9265b0e03a6a44a46e3e10c63
jntd <jntd@gft.com>
2021-11-19 15:41:20 -0300

    classe adicionada

---
+++ src/main/java/com/example/E_Commerce/model/Brand.java
@@ -0,0 +1,13 @@
+package com.example.E_Commerce.model;
+
+public class Brand {
+
+    private Long id;
+
+    private String marca_name;
+
+    private Long ean;
+
+    private Long value_name;
+
+}
