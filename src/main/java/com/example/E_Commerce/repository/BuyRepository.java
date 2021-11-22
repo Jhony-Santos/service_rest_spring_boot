a2768fb7261b7e718cd2b9eb565015b89c8cf9bd
jntd <jntd@gft.com>
2021-11-22 00:03:13 -0300

    Reestructed Buy Repository class

--- src/main/java/com/example/E_Commerce/repository/BuyRepository.java
+++ src/main/java/com/example/E_Commerce/repository/BuyRepository.java
@@ -1,7 +1,14 @@
 package com.example.E_Commerce.repository;
 
 import com.example.E_Commerce.model.Buy;
+import com.example.E_Commerce.model.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BuyRepository extends JpaRepository<Buy,Long> {
+
+    Optional<Buy> findBuyById(Long id);
+
+
 }
