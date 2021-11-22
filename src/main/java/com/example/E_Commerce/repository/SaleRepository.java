d9c974bc425bb9ddaa0678d3b20403fa2519068a
jntd <jntd@gft.com>
2021-11-22 00:38:38 -0300

    Restructured SaleRepository class

--- src/main/java/com/example/E_Commerce/repository/SaleRepository.java
+++ src/main/java/com/example/E_Commerce/repository/SaleRepository.java
@@ -3,5 +3,10 @@ package com.example.E_Commerce.repository;
 import com.example.E_Commerce.model.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SellRepository extends JpaRepository<Sale, Long> {
+import java.util.Optional;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    Optional<Sale> findSaleById(Long id);
+
 }
