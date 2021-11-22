2797dc6c803b73d33318bb1b517ef9c30112f431
jntd <jntd@gft.com>
2021-11-22 00:46:02 -0300

    Restructured StockRepository class

--- src/main/java/com/example/E_Commerce/repository/StockRepository.java
+++ src/main/java/com/example/E_Commerce/repository/StockRepository.java
@@ -1,11 +1,16 @@
+
 package com.example.E_Commerce.repository;
 
 import com.example.E_Commerce.model.Product;
 import com.example.E_Commerce.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
-    Stock findByProduto(Product product);
+    Stock findByProduct(Product product);
 
 }
+
+
